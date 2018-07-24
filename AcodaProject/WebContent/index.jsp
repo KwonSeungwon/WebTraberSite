6827b031deda9ada8e702f998c7ced3d82312de9
aydata <Playdata@DESKTOP-BGSPEB0>
2018-07-24 20:23:29 +0900

    asd

--- AcodaProject/WebContent/index.jsp
+++ AcodaProject/WebContent/index.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
 	pageEncoding="EUC-KR"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@page import = "com.acoda.vo.MemberVO" %>
+<%@page import="com.acoda.vo.MemberVO"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -11,24 +11,29 @@
 </head>
 <body>
 	<h2>**메인페이지**</h2>
-	<input type="button" value="회원리스트"
-		onclick="location.href ='input/Select.do'">
-	<input type="button" value="회원가입"
-		onclick="location.href ='input/insert.jsp'">
+
+	<%
+		MemberVO vo = (MemberVO) session.getAttribute("login");
+		if (session.getAttribute("login") != null) {
+	%>
 	<input type="button" value="회원탈퇴"
 		onclick="location.href ='input/UserDelete.jsp'">
+	<input type="button" value="로그아웃"
+		onclick="location.href ='/AcodaProject/logout.do'">
 	<input type="button" value="회원정보수정"
 		onclick="location.href ='input/UserChangeInfo.jsp'">
-		<%
-		   MemberVO vo = (MemberVO) session.getAttribute("login");			
-		if(session.getAttribute("login") != null){
-		%>		
-		<input type="button" value="로그아웃"
-		onclick="location.href ='/AcodaProject/logout.do'">
-	<%=vo.getId()+ "님 환영합니다!" %>	
-	<%}else{ %>
-		<input type="button" value="로그인"
+	<%=vo.getId() + "님 환영합니다!"%>
+	<%
+		} else {
+	%>
+	<input type="button" value="로그인"
 		onclick="location.href ='input/login_form.jsp'">
-	<%} %>
+	<input type="button" value="회원리스트" onclick="location.href ='Select.do'">
+	<input type="button" value="회원가입"
+		onclick="location.href ='input/UserInsert.jsp'">
+
+	<%
+		}
+	%>
 </body>
 </html>
--- AcodaProject/src/com/acoda/controller/UserInfoController.java
+++ AcodaProject/src/com/acoda/controller/UserInfoController.java
@@ -1,6 +1,12 @@
 package com.acoda.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.acoda.biz.UserInfoBiz;
+import com.acoda.dao.UserLoginDao;
 import com.acoda.vo.MemberVO;
 
 @Controller
@@ -23,9 +30,19 @@ public class UserInfoController {
 	@RequestMapping(value = "/abc.do", method = RequestMethod.POST)
 	public ModelAndView insert(@ModelAttribute("userInfo") MemberVO vo) {
 		System.out.println("컨트롤러 도착" + vo.getUser_number());
-		String result = infoBiz.insert(vo);
-		ModelAndView mav = new ModelAndView("UserView/InsertResult", "myresult", result);
-		// 출력파일 : result.jsp, 꺼내올 객체명(request객체) : myresult, 전송할 객체 : result
+		System.out.println(vo.getRating() + vo.getJoindate());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String today = sdf.format(new Date());
+		vo.setJoindate(today);
+		ModelAndView mav = new ModelAndView();
+
+		if (vo.getRating() == 2) {
+			String result = infoBiz.insert(vo);
+			mav = new ModelAndView("UserView/InsertResult", "myresult", result);
+		} else {
+			String result2 = infoBiz.insertNomal(vo);
+			mav = new ModelAndView("UserView/InsertResult", "myresult", result2);
+		}
 		return mav;
 	}
 
@@ -40,11 +57,52 @@ public class UserInfoController {
 
 	// 딜리트 컨트롤러
 	@RequestMapping(value = "deleteuser.do", method = RequestMethod.POST)
-	public ModelAndView delete(@ModelAttribute("userInfo") MemberVO vo) {
+	public ModelAndView delete(@ModelAttribute("userInfo") MemberVO vo, HttpServletResponse rep) throws IOException {
 		System.out.println("받은 아이디 :" + vo.getId() + vo.getPw());
 		ModelAndView mav = new ModelAndView("UserView/DeleteUserResult", "myid", vo);
-		infoBiz.delete(vo);
+		if (infoBiz.logincheck(vo) == 0) {
+			infoBiz.delete(vo);
+			System.out.println("삭제완료");
+
+		} else {
+			PrintWriter out = rep.getWriter();
+			out.println("<script type='text/javascript'>");
+			out.println("alert('Please Check your ID&PW');");
+			out.println("history.back();");
+			out.println("</script>");
+			System.out.println("삭제실패");
+			out.flush();
+			out.close();
+
+		}
 		return mav;
 	}
+	
+	@RequestMapping(value = "/idcheck.do", method = RequestMethod.GET)
+	public void Idcheck (HttpServletResponse response, ModelAndView mav,MemberVO vo) throws IOException {
+		PrintWriter out = response.getWriter();
+		System.out.println("검사한 ID" + vo.getId());
+		if(infoBiz.checkId(vo) == 0) {
+			out.println("<script type='text/javascript'>");
+			out.println("alert('Used ID!');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.flush();
+			out.close();
+			
+		}else {
+			out.println("<script type='text/javascript'>");
+			out.println("alert('Not Used ID!');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.flush();
+			out.close();
+		}
+		
+		
+	}
+
+	
+	
 
 }
